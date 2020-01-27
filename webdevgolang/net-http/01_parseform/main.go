8d7ff4d61bc60e4b1cd7981f687761042d67425d
adionisiodr <adionisio82@gmail.com>
2020-01-27 00:51:30 -0500

    simple form read

---
+++ webdevgolang/net-http/01_parseform/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type simpleServer int
+
+func (m simpleServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d simpleServer
+	http.ListenAndServe(":8080", d)
+}
