bd090eebfa1a9c2bf97861e5c3d4c070faa8af23
adionisiodr <adionisio82@gmail.com>
2020-03-20 22:50:01 -0400

    how to handle url search query

--- golang/webdevgolang/routing/main.go
+++ golang/webdevgolang/routing/main.go
@@ -13,9 +13,16 @@ func catHandler(res http.ResponseWriter, req *http.Request) {
 	io.WriteString(res, "cat cat cat")
 }
 
+// http://localhost:8080/search?q=serve_me_this_data
+func handleURLSearchQuery(res http.ResponseWriter, req *http.Request) {
+	v := req.FormValue("q")
+	io.WriteString(res, "Query value 'q':"+v)
+}
+
 func main() {
 	http.HandleFunc("/dog/", dogHandler)
 	http.HandleFunc("/cat", catHandler)
+	http.HandleFunc("/search", handleURLSearchQuery)
 
 	http.ListenAndServe(":8080", nil)
 }
